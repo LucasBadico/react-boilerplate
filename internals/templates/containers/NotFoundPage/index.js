01dc60848fe71e5776e1c3e6c40ab3dabc06a690
Lucas Badico <lucasgomesdesantana@gmail.com>
2020-03-25 21:37:41 -0300

    feat: copy src/views into template containers

--- internals/templates/containers/NotFoundPage/index.js
+++ internals/templates/containers/NotFoundPage/index.js
@@ -2,18 +2,20 @@
  * NotFoundPage
  *
  * This is the page we show when the user visits a url that doesn't have a route
- *
  */
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import messages from './messages';
+import H1 from 'components/H1';
+import messages from './translations';
 
 export default function NotFound() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <article>
+      <H1>
+        <FormattedMessage {...messages.header} />
+      </H1>
+    </article>
   );
 }
